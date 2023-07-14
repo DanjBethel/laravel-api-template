51c2c9e39cf8e5593c83cd30f1b4898bafbad1fa
DanjBethel <danjbethel@gmail.com>
2023-07-14 16:48:11 -0400

    changed uuid to ulids

--- src/Domain/User/Models/User.php
+++ src/Domain/User/Models/User.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Domain\User\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -15,7 +15,7 @@ final class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
-    use HasUuids;
+    use HasUlids;
     use Notifiable;
 
     protected $fillable = [
