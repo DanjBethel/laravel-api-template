2c3f6af254a75226fffa6ad8f26a7ed4e031b549
DanjBethel <danjbethel@gmail.com>
2023-07-16 22:06:38 -0400

    changed default routes file again

--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -5,17 +5,22 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 /**
- * Ping Routes
- */
-Route::prefix('ping')->as('ping:')->group(
-    base_path('routes/api/v1/ping.php'),
-);
-
-/**
  * Authenticated Routes
  */
-//Route::middleware(middleware: ['auth:api'])->group(static function (): void {
-//});
+Route::middleware(['auth:api'])->group(static function (): void {
+    //
+});
 
+/**
+ * Unauthenticated Routes
+ */
+Route::middleware([])->group(static function (): void {
+    /**
+     * Ping Routes
+     */
+    Route::prefix('ping')->as('ping:')->group(
+        base_path('routes/api/v1/ping.php'),
+    );
+});
 
 require __DIR__.'/../../auth.php';
